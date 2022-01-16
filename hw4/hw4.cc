4374c266bad832e74c5392f00d82e9ce56ee091c
yuan0520 <abcdabcd10211@gmail.com>
2022-01-16 11:48:48 +0800

    pass all testcases

--- hw4/hw4.cc
+++ hw4/hw4.cc
@@ -14,6 +14,9 @@
 #include <queue>
 #include <unistd.h>
 
+#define ascending 0
+#define descending 1
+
 using namespace std;
 
 typedef std::pair<std::string, int> Item;
@@ -33,38 +36,80 @@ string reduce_file_dir = "./reduce_file/";
 string locality_config_filename;
 
 int stop_mapping;
+int stop_reducing;
 int thread_available;
 int ncpus;
 int queue_size;
 int inter_file_id = 1;
-queue<int> chunk_waiting_queue;
+queue<pair<int, int>> chunk_waiting_queue;
+
+queue<pair<int, int>> mapper_complete_info;
+queue<pair<int, int>> reducer_complete_info;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex3 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_inter = PTHREAD_MUTEX_INITIALIZER;
 
+int sorting = ascending;
+
+struct cmp_{
+    bool operator()(const string& k1, const string& k2) const{
+        if(sorting == ascending){
+            return k1 < k2;
+        }
+        else{
+            return k1 > k2;
+        }
+    }
+};
+
+int cal_time(struct timespec start_time, struct timespec end_time)
+{
+    struct timespec temp;
+    if ((end_time.tv_nsec - start_time.tv_nsec) < 0) {
+        temp.tv_sec = end_time.tv_sec-start_time.tv_sec-1;
+        temp.tv_nsec = 1000000000 + end_time.tv_nsec - start_time.tv_nsec;
+    } else {
+        temp.tv_sec = end_time.tv_sec - start_time.tv_sec;
+        temp.tv_nsec = end_time.tv_nsec - start_time.tv_nsec;
+    }
+    double exe_time = temp.tv_sec + (double) temp.tv_nsec / 1000000000.0;
+    return (int)(ceil(exe_time));
+}
+
 void* mapper(void* t){
     int* tid = (int*)t;
     int chunkID;
+    int flag;
+    pair<int, int> p;
+
+    struct timespec start_time, end_time, temp;
+    int chunk_time;
 
     while(1){
         pthread_mutex_lock(&mutex);
         if(queue_size > 0){
-            chunkID = chunk_waiting_queue.front();
+            p = chunk_waiting_queue.front();
             chunk_waiting_queue.pop();
             queue_size--;
             thread_available--;
             pthread_mutex_unlock(&mutex);
 
-            int start_pos = chunkID * chunk_size - 1; // read text begin from #start_pos line
-            int i = 1;
+            clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+            if(p.second == 1) sleep(delay);
+
+            chunkID = p.first;
+            // cout << "chunkID: " << chunkID << endl;
+            int start_pos = (chunkID-1) * chunk_size; // read text begin from #start_pos line
+            int i = 0;
             std::ifstream input_file(input_filename);
             string input_line; 
             queue<record> records;
             map<string, int> word_count;
 
             // input split function
-            while(i < start_pos){
+            while(i != start_pos){
                 getline(input_file, input_line);
                 i++;
             }
@@ -85,36 +130,59 @@ void* mapper(void* t){
                 vector<string> words;
                 string word;
 
+                // while ((pos = l.find(" ")) != std::string::npos){
+                //     word = l.substr(0, pos);
+                //     words.push_back(word);
+
+                //     l.erase(0, pos + 1);
+                // }
+                // if (!l.empty())
+                //     words.push_back(l);
+
+                // for (auto word : words){
+                //     if (word_count.count(word) == 0){
+                //         word_count[word] = 1;
+                //     }
+                //     else{
+                //         word_count[word]++;
+                //     }
+                // }    
                 while ((pos = l.find(" ")) != std::string::npos){
                     word = l.substr(0, pos);
-                    words.push_back(word);
-
-                    l.erase(0, pos + 1);
-                }
-                if (!l.empty())
-                    words.push_back(l);
-
-                for (auto word : words){
                     if (word_count.count(word) == 0){
                         word_count[word] = 1;
                     }
                     else{
                         word_count[word]++;
                     }
-                }    
+
+                    l.erase(0, pos + 1);
+                }
+
+                if (word_count.count(l) == 0){
+                    word_count[l] = 1;
+                }
+                else{
+                    word_count[l]++;
+                }
             }
 
-            pthread_mutex_lock(&mutex_inter);
-            std::ofstream out(inter_dir + "inter" + "-" + std::to_string(inter_file_id) + ".out");
+            
+            std::ofstream out(inter_dir + "inter" + "-" + std::to_string(chunkID) + ".out");
             for(Item item : word_count){
                 out << item.first << " " << item.second << "\n";
             }
+
+            pthread_mutex_lock(&mutex_inter);
             inter_file_id++;
             pthread_mutex_unlock(&mutex_inter);
 
+            clock_gettime(CLOCK_MONOTONIC, &end_time);
+            chunk_time = cal_time(start_time, end_time);
+
             pthread_mutex_lock(&mutex);
+            mapper_complete_info.push(make_pair(chunkID, chunk_time));
             thread_available++;
-            // cout << "thread_avai " << thread_available << ", stop: " << stop << ", thread: " << *tid << ", chunk: " << chunkID << ", line: " << records.front().first << ", text: " << records.front().second << endl;
             pthread_mutex_unlock(&mutex);
         }
         else{
@@ -122,15 +190,10 @@ void* mapper(void* t){
         }
 
         if(stop_mapping == 1 && inter_file_id == total_chunk + 1 && thread_available == ncpus - 1){
-            // cout << "thread: " << *tid << ", inter_id: " << inter_file_id << ", total chunk: " << total_chunk << endl;
+            
             break; 
         } 
     }
-    pthread_exit(NULL);
-}
-
-void* reducer(void* t){
-    cout << "reducer" << endl;
     pthread_exit(NULL);
 }
 
@@ -161,6 +224,11 @@ int main(int argc, char **argv)
 
     // job tracker
     if(rank == size - 1){
+        struct timespec start_time, end_time;
+        clock_gettime(CLOCK_MONOTONIC, &start_time);
+        std::ofstream log_out(output_dir + job_name + "-log.out");
+        log_out << time(nullptr) << ",Start_Job," << job_name << "," << size << "," << ncpus << "," << num_reducer << "," << delay << "," << input_filename << "," << chunk_size << "," << locality_config_filename << "," << output_dir << endl;
+
         std::ifstream locality_file(locality_config_filename);
         string locality_line;
         std::vector<int_int> chunk_queue;
@@ -174,7 +242,7 @@ int main(int argc, char **argv)
 
             pos = locality_line.find(" ");
             chunk_id = stoull(locality_line.substr(0, pos));
-            chunk_pos = stoull(locality_line.substr(pos+1, line_len-pos-1)) % num_task_tracker;
+            chunk_pos = stoull(locality_line.substr(pos+1)) % num_task_tracker;
             
             c.first = chunk_id;
             c.second = chunk_pos;
@@ -186,26 +254,32 @@ int main(int argc, char **argv)
         // while there is still some chunk not being handled
         while(!chunk_queue.empty()){
             int from_node; // whose request
-            int chunk_to_give = 0; // which chunk to give
+            int chunk_to_give[3]; // which chunk to give
             int index; // the position of the chunk in the queue
             int flag = 0;
             MPI_Status status;
             MPI_Recv(&from_node, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
 
             for(int i = 0; i < chunk_queue.size(); i++){
-                if(chunk_queue.at(i).second == status.MPI_SOURCE){
-                    chunk_to_give = chunk_queue.at(i).first;
+                if(chunk_queue.at(i).second == from_node){
+                    chunk_to_give[0] = chunk_queue.at(i).first;
+                    chunk_to_give[1] = chunk_queue.at(i).second;
+                    chunk_to_give[2] = from_node;
                     index = i;
                     flag = 1;
                     break;
                 }
             }
             if(flag == 0){
-                chunk_to_give = chunk_queue.front().first;
+                chunk_to_give[0] = chunk_queue.front().first;
+                chunk_to_give[1] = chunk_queue.front().second;
+                chunk_to_give[2] = from_node;
                 index = 0;
+                // sleep(delay);
             }
 
-            MPI_Send(&chunk_to_give, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
+            log_out << time(nullptr) << ",Dispatch_MapTask," << chunk_to_give[0] << "," << from_node << endl;
+            MPI_Send(&chunk_to_give, 3, MPI_INT, from_node, 0, MPI_COMM_WORLD);
             chunk_queue.erase(chunk_queue.begin() + index);
         }
 
@@ -213,6 +287,14 @@ int main(int argc, char **argv)
         stop_mapping = -1;
         for(int i = 0; i < size - 1; i++){
             MPI_Send(&stop_mapping, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+
+        int count = 0;
+        int chunk_info[2];
+        while(count < total_chunk){
+            MPI_Recv(&chunk_info, 2, MPI_INT, MPI_ANY_SOURCE, 5, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            log_out << time(nullptr) << ",Complete_MapTask," << chunk_info[0] << "," << chunk_info[1] << "\n";
+            count++;
         }
 
         int start_partition = 0;
@@ -223,6 +305,11 @@ int main(int argc, char **argv)
           partition function
         **********************
         */
+
+        struct timespec start_time_shuffle, end_time_shuffle;
+        clock_gettime(CLOCK_MONOTONIC, &start_time_shuffle);
+        log_out << time(nullptr) << ",Start_Shuffle," << to_string(total_chunk) << endl;
+
         vector<string_int> inter_pair_list[num_reducer];
         for(int c = 1; c <= total_chunk; c++){
             string path = inter_dir + "inter-" + std::to_string(c) + ".out";
@@ -238,8 +325,7 @@ int main(int argc, char **argv)
 
                 pos = inter_line.find(" ");
                 key = inter_line.substr(0, pos);
-                string v = inter_line.substr(pos+1, inter_line_len-pos-1);
-                value = stoi(v);
+                value = stoi(inter_line.substr(pos+1));
                 o.first = key;
                 o.second = value;
                 int which_reducer = (key[0] - 'A') % num_reducer;
@@ -248,11 +334,18 @@ int main(int argc, char **argv)
         }
 
         for(int f = 0; f < num_reducer; f++){
-            std::ofstream out(reduce_file_dir + "reduce" + "-" + std::to_string(f) + ".out");
+            std::ofstream out(reduce_file_dir + "reduce" + "-" + std::to_string(f+1) + ".out");
             for(string_int item : inter_pair_list[f]){
+                // if(item.first == "Aulacomnium"){
+                //     cout << "Aulacomnium: " << item.second << endl;
+                // }
                 out << item.first << " " << item.second << "\n";
             }
         }
+
+        clock_gettime(CLOCK_MONOTONIC, &end_time_shuffle);
+        int shuffle_time = cal_time(start_time_shuffle, end_time_shuffle);
+        log_out << time(nullptr) << ",Finish_Shuffle," << to_string(shuffle_time) << endl;
 
         // dispatch reduce task
         for(int r = 0; r < num_reducer; r++){
@@ -260,9 +353,27 @@ int main(int argc, char **argv)
             int task_num = r;
             MPI_Status status;
             MPI_Recv(&from_node, 1, MPI_INT, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, &status);
-            MPI_Send(&task_num, 1, MPI_INT, from_node, 3, MPI_COMM_WORLD);
+            MPI_Send(&task_num, 1, MPI_INT, from_node, 7, MPI_COMM_WORLD);
+            log_out << time(nullptr) << ",Dispatch_ReduceTask," << task_num+1 << "," << from_node << endl;
         }
 
+        stop_reducing = -1;
+        for(int t = 0; t < size-1; t++){
+            int from;
+            MPI_Recv(&from, 1, MPI_INT, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(&stop_reducing, 1, MPI_INT, from, 7, MPI_COMM_WORLD);
+        }
+
+        int count_reduce = 0;
+        int reduce_info[2];
+        while(count_reduce < num_reducer){
+            MPI_Recv(&reduce_info, 2, MPI_INT, MPI_ANY_SOURCE, 6, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            log_out << time(nullptr) << ",Complete_ReduceTask," << reduce_info[0] << "," << reduce_info[1] << "\n";
+            count_reduce++;
+        }
+        clock_gettime(CLOCK_MONOTONIC, &end_time);
+        int total_program_time = cal_time(start_time, end_time);
+        log_out << time(nullptr) << ",Finish_Job," << to_string(total_program_time) << endl;
     }
 
     /*
@@ -272,7 +383,7 @@ int main(int argc, char **argv)
     */
     else{
         int rc;
-        int chunk_to_process; // the chunk to process
+        int chunk_to_process[3]; // the chunk to process
         thread_available = ncpus - 1;
         stop_mapping = 0;
         queue_size = 0;
@@ -294,17 +405,22 @@ int main(int argc, char **argv)
 
         while(1){
             pthread_mutex_lock(&mutex);
+            int flag = 0;
             if(thread_available > 0){
                 MPI_Send(&rank, 1, MPI_INT, size-1, 0, MPI_COMM_WORLD);
-                MPI_Recv(&chunk_to_process, 1, MPI_INT, size-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                if(chunk_to_process == -1){
+                MPI_Recv(&chunk_to_process, 3, MPI_INT, size-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                if(chunk_to_process[1] != chunk_to_process[2]){
+                    flag = 1;
+                };
+                if(chunk_to_process[0] == -1){
                     stop_mapping = 1;
                     pthread_mutex_unlock(&mutex);
                 }
                 else{
-                    chunk_waiting_queue.push(chunk_to_process);
+                    chunk_waiting_queue.push(make_pair(chunk_to_process[0], flag));
                     queue_size++;
                     total_chunk++;
+                    // cout << "get job by process: " << rank << ", chunk ID: " << chunk_to_process[0] << ", flag: " << flag << endl;
                     pthread_mutex_unlock(&mutex);
                 } 
             }
@@ -312,6 +428,15 @@ int main(int argc, char **argv)
             if(stop_mapping){
                 for(int i = 0; i < ncpus - 1; i++){
                     pthread_join(mapper_threads[i], NULL);
+                }
+
+                while(!mapper_complete_info.empty()){
+                    int chunk_info[2];
+                    pair<int, int> info = mapper_complete_info.front();
+                    mapper_complete_info.pop();
+                    chunk_info[0] = info.first;
+                    chunk_info[1] = info.second;
+                    MPI_Send(&chunk_info, 2, MPI_INT, size-1, 5, MPI_COMM_WORLD);
                 }
                 if(rank == 0){
                     int start_partition = 1;
@@ -326,13 +451,22 @@ int main(int argc, char **argv)
                 reducer
         **********************
         */
+        int local_reduce_tasks = 0;
         while(1){
             int task_num;
             MPI_Send(&rank, 1, MPI_INT, size-1, 3, MPI_COMM_WORLD);
-            MPI_Recv(&task_num, 1, MPI_INT, size-1, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            // cout << "rank: " << rank << ", taskID: " << task_num << endl;
+            MPI_Recv(&task_num, 1, MPI_INT, size-1, 7, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-            string reduce_path = reduce_file_dir + "reduce" + "-" + std::to_string(task_num) + ".out";
+            struct timespec start_time_reduce, end_time_reduce;
+            clock_gettime(CLOCK_MONOTONIC, &start_time_reduce);
+
+            if(task_num == -1){
+                break;
+                // MPI_Finalize();
+                // return 0;
+            } 
+
+            string reduce_path = reduce_file_dir + "reduce" + "-" + std::to_string(task_num+1) + ".out";
             std::ifstream reduce_file(reduce_path);
             vector<string_int> reduce_strings;
             string reduce_string;
@@ -357,8 +491,16 @@ int main(int argc, char **argv)
                 sorting function
             **********************
             */
-            std::sort(reduce_strings.begin(), reduce_strings.end(), [](const string_int &item1, const string_int &item2) -> bool
+            if(sorting == ascending){
+                std::sort(reduce_strings.begin(), reduce_strings.end(), [](const string_int &item1, const string_int &item2) -> bool
                 {return item1.first < item2.first;});
+            }
+            else{
+                std::sort(reduce_strings.begin(), reduce_strings.end(), [](const string_int &item1, const string_int &item2) -> bool
+                {return item1.first > item2.first;});
+            }
+            
+            // std::sort(reduce_strings.rbegin(), reduce_strings.rend());
 
 
             /*
@@ -367,7 +509,7 @@ int main(int argc, char **argv)
             **********************
             */
             vector<string_int>::iterator it;
-            map<string, string> group_result;
+            map<string, string, cmp_> group_result;
             for(it = reduce_strings.begin(); it != reduce_strings.end(); it++){
                 string key = it.base()->first;
                 int value = it.base()->second;
@@ -385,8 +527,8 @@ int main(int argc, char **argv)
                 reduce function
             **********************
             */
-            map<string, int> reduce_map;
-            map<string, string>::iterator iter = group_result.begin();
+            map<string, int, cmp_> reduce_map;
+            map<string, string, cmp_>::iterator iter = group_result.begin();
             while(iter != group_result.end()){
                 int sum = 0;
                 int pos = 0;
@@ -407,23 +549,35 @@ int main(int argc, char **argv)
                 iter++;
             }
 
+            clock_gettime(CLOCK_MONOTONIC, &end_time_reduce);
+            int reduce_time = cal_time(start_time_reduce, end_time_reduce);
+            reducer_complete_info.push(make_pair(task_num, reduce_time));
+            local_reduce_tasks++;
+
             /*
             **********************
                 output function
             **********************
             */
 
-            std::ofstream out(output_dir + job_name + "-" + std::to_string(task_num) + ".out");
-            map<string, int>::iterator iter_output = reduce_map.begin();
+            std::ofstream out(output_dir + job_name + "-" + std::to_string(task_num+1) + ".out");
+            map<string, int, cmp_>::iterator iter_output = reduce_map.begin();
             while(iter_output != reduce_map.end()){
                 out << iter_output->first << " " << iter_output->second << '\n';
                 iter_output++;
             }
-
         }
-        
+        int reduce_count = 0;
+        while(reduce_count < local_reduce_tasks){
+            int reduce_info[2];
+            pair<int, int> p = reducer_complete_info.front();
+            reducer_complete_info.pop();
+            reduce_info[0] = p.first;
+            reduce_info[1] = p.second;
+            MPI_Send(&reduce_info, 2, MPI_INT, size-1, 6, MPI_COMM_WORLD);
+            reduce_count++;
+        }
     }
-
-
+    MPI_Finalize();
     return 0;
 }
--- hw4/test.sh
+++ hw4/test.sh
@@ -1,5 +1,23 @@
-make clean
-make 
-
+make
+g++ -o judge judge.cc
+rm output/*.out
 # srun -N2 -c5 ./hw4 TEST01 7 3 testcases/01.word 2 testcases/01.loc output/
-srun -N3 -c12 ./hw4 TEST05 10 1 testcases/05.word 2 testcases/05.loc output/
+# ./judge output/TEST01 7 testcases/01.ans
+# srun -N2 -c12 ./hw4 TEST02 7 2 testcases/02.word 2 testcases/02.loc output/
+# ./judge output/TEST02 7 testcases/02.ans
+# srun -N3 -c10 ./hw4 TEST03 10 2 testcases/03.word 5 testcases/03.loc output/
+# ./judge output/TEST03 10 testcases/03.ans
+# srun -N3 -c12 ./hw4 TEST04 10 3 testcases/04.word 5 testcases/04.loc output/
+# ./judge output/TEST04 10 testcases/04.ans
+# srun -N3 -c12 ./hw4 TEST05 10 1 testcases/05.word 2 testcases/05.loc output/
+# ./judge output/TEST05 10 testcases/05.ans
+# srun -N4 -c10 ./hw4 TEST06 12 3 testcases/06.word 3 testcases/06.loc output/
+# ./judge output/TEST06 12 testcases/06.ans
+# srun -N4 -c12 ./hw4 TEST07 13 3 testcases/07.word 4 testcases/07.loc output/
+# ./judge output/TEST07 13 testcases/07.ans
+# srun -N4 -c12 ./hw4 TEST08 12 3 testcases/08.word 6 testcases/08.loc output/
+# ./judge output/TEST08 12 testcases/08.ans
+# srun -N4 -c12 ./hw4 TEST09 7 3 testcases/09.word 5 testcases/09.loc output/
+# ./judge output/TEST09 7 testcases/09.ans
+srun -N4 -c12 ./hw4 TEST10 12 5 testcases/10.word 10 testcases/10.loc output/
+./judge output/TEST10 12 testcases/10.ans
